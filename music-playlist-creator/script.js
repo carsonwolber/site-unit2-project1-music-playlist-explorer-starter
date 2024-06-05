a2d66854d044041bb701fcd230438ed760bdd66b
Carson Wolber <cwolber@meta.com>
2024-06-05 16:42:50 -0700

    end of day 3. up to milestone 3

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    playlistLoader();
+})
+
+function playlistLoader() {
+    const playlistSet = document.getElementById('playlist-cards');
+    data.playlists.forEach(playlist => {
+        const playlistElement = createPlaylistCard(playlist);
+        playlistSet.appendChild(playlistElement);
+    });
+
+}
+
+function createPlaylistCard(playlist) { 
+    const div = document.createElement('div')
+    div.className = 'playlist-card'
+    div.innerHTML = `
+    <img id="playlist-img" src = ${playlist.playlist_art}/>
+    <h3 id="playlist-title">${playlist.playlist_name}</h3> 
+    <p id="playlist-name">${playlist.playlist_creator}</p>
+    <p id="count">${playlist.likeCount}</p?>
+    `;
+    return div;
+}
 
