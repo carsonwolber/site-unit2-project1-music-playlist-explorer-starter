8de99de52119005116f6bfc6261a7a4e55f2af22
Carson Wolber <cwolber@meta.com>
2024-06-06 16:56:37 -0700

    end of day push

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     playlistLoader();
 })
 
+
+
+
+
+
 function playlistLoader() {
     const playlistSet = document.getElementById('playlist-cards');
     data.playlists.forEach(playlist => {
@@ -18,6 +23,7 @@ function createPlaylistCard(playlist) {
     <img id="playlist-img" src = ${playlist.playlist_art}/>
     <h3 id="playlist-title">${playlist.playlist_name}</h3> 
     <p id="playlist-name">${playlist.playlist_creator}</p>
+    <img id="likebtn" src="assets/img/like.png"/>
     <p id="count">${playlist.likeCount}</p?>
     `;
     return div;
